cf15ee563ea8c4bb00b7c8d6e800e8a7370b68be
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-08-01 20:38:26 -0400

    chore: analyze types distribution
    - https://github.com/arethetypeswrong/arethetypeswrong.github.io
    
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,27 +1,38 @@
 #!/bin/sh
 
 # Local Release Workflow
-
+#
 # 1. run typecheck
 # 2. run tests
 # 3. pack project
 # 4. run postbuild typecheck
-# 5. get new package version
-# 6. get release branch name
-# 7. switch to release branch
-# 8. stage changes
-# 9. commit changes
-# 10. push release branch to origin
-# 11. cleanup
+# 5. analyze types
+# 6. print package size report
+# 7. get new package version
+# 8. get release branch name
+# 9. switch to release branch
+# 10. stage changes
+# 11. commit changes
+# 12. push release branch to origin
+# 13. create pull request
+# 14. cleanup
+#
+# References:
+#
+# - https://cli.github.com/manual/gh_pr_create
+# - https://github.com/arethetypeswrong/arethetypeswrong.github.io
 
 yarn typecheck
 yarn test:cov
-yarn pack -o %s-%v.tgz
+yarn pack
 yarn check:types:build
+attw package.tgz
+yarn pkg-size
 VERSION=$(jq .version package.json -r)
 RELEASE_BRANCH=release/$VERSION
 git switch -c $RELEASE_BRANCH
 git add .
-git commit -s -m "release: $(jq .tagPrefix package.json -r)$VERSION" --no-verify
+git commit -s -m "release: $(jq .tagPrefix package.json -r)$VERSION"
 git push origin -u --no-verify $RELEASE_BRANCH
+gh pr create --assignee @me --label scope:release --web
 yarn clean:pack
